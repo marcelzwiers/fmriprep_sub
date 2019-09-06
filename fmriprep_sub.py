7fe36a2126baea188c33c596fbcaeed9781011d6
MarsMellow <mpzwiers@gmail.com>
2019-09-06 14:36:48 +0200

    Removed outputspace because it is not compatible with the new (more complicated) way templates are handled in v1.4.1 interface

--- fmriprep_sub.py
+++ fmriprep_sub.py
@@ -12,7 +12,7 @@ import subprocess
 import uuid
 
 
-def main(bidsdir, outputdir, workdir_, outputspace, subject_label=(), force=False, mem_mb=18000, argstr='', dryrun=False, skip=True):
+def main(bidsdir, outputdir, workdir_, subject_label=(), force=False, mem_mb=18000, argstr='', dryrun=False, skip=True):
 
     # Default
     if not outputdir:
@@ -62,7 +62,7 @@ def main(bidsdir, outputdir, workdir_, outputspace, subject_label=(), force=Fals
             # Submit the job to the compute cluster
             command = """qsub -l walltime=70:00:00,mem={mem_mb}mb -N fmriprep_{sub_id} <<EOF
                          module add fmriprep; cd {pwd}
-                         {fmriprep} {bidsdir} {outputdir} participant -w {workdir} --participant-label {sub_id} --output-space {outputspace} --skip-bids-validation --fs-license-file {licensefile} --mem_mb {mem_mb} --omp-nthreads 1 --nthreads 1 {args}
+                         {fmriprep} {bidsdir} {outputdir} participant -w {workdir} --participant-label {sub_id} --skip-bids-validation --fs-license-file {licensefile} --mem_mb {mem_mb} --omp-nthreads 1 --nthreads 1 {args}
                          {cleanup}\nEOF"""\
                          .format(pwd         = os.getcwd(),
                                  fmriprep    = f'unset PYTHONPATH; export PYTHONNOUSERSITE=1; singularity run {os.getenv("DCCN_OPT_DIR")}/fmriprep/{os.getenv("FMRIPREP_VERSION")}/fmriprep-{os.getenv("FMRIPREP_VERSION")}.simg',
@@ -70,7 +70,6 @@ def main(bidsdir, outputdir, workdir_, outputspace, subject_label=(), force=Fals
                                  outputdir   = outputdir,
                                  workdir     = workdir,
                                  sub_id      = sub_id,
-                                 outputspace = ' '.join(outputspace),
                                  licensefile = os.getenv('FS_LICENSE'),
                                  mem_mb      = mem_mb,
                                  args        = argstr,
@@ -109,10 +108,11 @@ if __name__ == "__main__":
                                             '  module help fmriprep\n'
                                             '  fmriprep -h\n\n'
                                             'examples:\n'
-                                            '  fmriprep_sub.py /project/3017065.01/bids\n'
-                                            '  fmriprep_sub.py /project/3017065.01/bids -w /project/3017065.01/fmriprep_work\n'
-                                            '  fmriprep_sub.py /project/3017065.01/bids -o /project/3017065.01/fmriprep --participant_label sub-P010 sub-P018\n'
-                                            '  fmriprep_sub.py /project/3017065.01/bids -a " --fs-no-reconall"\n'
+                                            '  fmriprep_sub.py /project/3017065.01/bids -a " --output-spaces MNI152Lin"\n'
+                                            '  fmriprep_sub.py /project/3017065.01/bids -a " --outputspace template" (use for v1.2, deprecated in v1.4)\n'
+                                            '  fmriprep_sub.py /project/3017065.01/bids -w /project/3017065.01/fmriprep_work -a " --output-spaces MNI152Lin"\n'
+                                            '  fmriprep_sub.py /project/3017065.01/bids -o /project/3017065.01/fmriprep --participant_label sub-P010 sub-P018 -a " --output-spaces MNI152Lin"\n'
+                                            '  fmriprep_sub.py /project/3017065.01/bids -a " --fs-no-reconall --output-spaces MNI152Lin"\n'
                                             '  fmriprep_sub.py /project/3017065.01/bids -a " --use-aroma --ignore slicetiming"\n'
                                             '  fmriprep_sub.py -f -m 40000 /project/3017065.01/bids -p P018\n\n'
                                             'author:\n'
@@ -121,12 +121,11 @@ if __name__ == "__main__":
     parser.add_argument('-o','--outputdir',         help='The output-directory where the frmiprep output is stored (None -> bidsdir/derivatives)')
     parser.add_argument('-w','--workdir',           help='The working-directory where intermediate files are stored (None -> temporary directory')
     parser.add_argument('-p','--participant_label', help='Space seperated list of sub-# identifiers to be processed (the sub- prefix can be removed). Otherwise all sub-folders in the bidsfolder will be processed', nargs='+')
-    parser.add_argument('-s','--outputspace',       help='Spatial coordinate system where the functional series are resample into (for more info: fmriprep -h)', default=['template'], choices=['T1w','template','fsnative','fsaverage','fsaverage6','fsaverage5'], nargs='+')
     parser.add_argument('-f','--force',             help='If this flag is given subjects will be processed, regardless of existing folders in the bidsfolder. Otherwise existing folders will be skipped', action='store_true')
     parser.add_argument('-i','--ignore',            help='If this flag is given then already running or scheduled jobs with the same name are ignored, otherwise job submission is skipped', action='store_false')
     parser.add_argument('-m','--mem_mb',            help='Maximum required amount of memory', default=18000, type=int)
-    parser.add_argument('-a','--args',              help='Additional arguments that are passed to fmriprep (NB: Use quotes to prevent parsing of spaces)', type=str, default='')
+    parser.add_argument('-a','--args',              help='Additional arguments that are passed to fmriprep (NB: Use quotes and a leading space to prevent unintended parsing)', type=str, default='')
     parser.add_argument('-d','--dryrun',            help='Add this flag to just print the fmriprep qsub commands without actually submitting them (useful for debugging)', action='store_true')
     args = parser.parse_args()
 
-    main(bidsdir=args.bidsdir, outputdir=args.outputdir, workdir_=args.workdir, outputspace=args.outputspace, subject_label=args.participant_label, force=args.force, mem_mb=args.mem_mb, argstr=args.args, dryrun=args.dryrun, skip=args.ignore)
+    main(bidsdir=args.bidsdir, outputdir=args.outputdir, workdir_=args.workdir, subject_label=args.participant_label, force=args.force, mem_mb=args.mem_mb, argstr=args.args, dryrun=args.dryrun, skip=args.ignore)
