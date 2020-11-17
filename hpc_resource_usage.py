fb453277e556b9c94742e3ce512af4169a2d5bc3
MarsMellow <mpzwiers@gmail.com>
2020-11-17 18:43:01 +0100

    Added the number of bins as input argument

--- hpc_resource_usage.py
+++ hpc_resource_usage.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 
-def main(datadirs: list, maxwalltime_: float, maxmem_: float):
+def main(datadirs: list, maxwalltime_: float, maxmem_: float, bins: int):
 
     # Parse the walltime and memory usage
     maxwalltime = 0
@@ -42,8 +42,8 @@ def main(datadirs: list, maxwalltime_: float, maxmem_: float):
     if len(datadirs) == 1:
         axs = axs.reshape(1, 2)
     for n, datadir in enumerate(datadirs):
-        axs[n,0].hist(walltime[datadir], bins=100, range=(0,min(maxwalltime,maxwalltime_)))
-        axs[n,1].hist(     mem[datadir], bins=100, range=(0,min(maxmem,maxmem_)))
+        axs[n,0].hist(walltime[datadir], bins=bins, range=(0,min(maxwalltime,maxwalltime_)))
+        axs[n,1].hist(     mem[datadir], bins=bins, range=(0,min(maxmem,maxmem_)))
         axs[n,0].set_ylabel(datadir.name)
     axs[-1,0].set_xlabel('Walltime (hour)')
     axs[-1,1].set_xlabel('Memory (Gb)')
@@ -61,10 +61,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description=__doc__)
     parser.add_argument('-w','--walltime', help='Maximum amount of used walltime (in hour) that is shown in the plots', type=float, default=float('Inf'))
     parser.add_argument('-m','--mem',      help='Maximum amount of used memory (in Gb) that is shown in the plots', type=float, default=float('Inf'))
+    parser.add_argument('-b','--bins',     help='Number of bins that are shown in the plots', type=int, default=100)
     parser.add_argument('datafolders',     help='Space separated list of folders containing "*.o*" PBS-logfiles. It is assumed that the logfiles contain a line similar to "Used resources:	   cput=03:22:23,walltime=01:01:53,mem=17452716032b". Each folder is plotted as a separate row (indicated by the foldername). Try "demo" for plotting fmriprep demo data', nargs='*', default='.')
     args = parser.parse_args()
 
     if args.datafolders == ['demo']:
         args.datafolders = ['/opt/fmriprep/dccn/nthreads = 1', '/opt/fmriprep/dccn/nthreads = 2', '/opt/fmriprep/dccn/nthreads = 4', '/opt/fmriprep/dccn/nthreads = 8']
 
-    main(datadirs=[Path(datadir) for datadir in args.datafolders], maxwalltime_=args.walltime, maxmem_=args.mem)
+    main(datadirs=[Path(datadir) for datadir in args.datafolders], maxwalltime_=args.walltime, maxmem_=args.mem, bins=args.bins)
