a7e46357a53537f3ce9413d102d7bc5b07aa0c57
MarsMellow <mpzwiers@gmail.com>
2023-11-02 17:29:15 +0100

    Add a user tip

--- fmriprep_sub.py
+++ fmriprep_sub.py
@@ -122,7 +122,10 @@ def main(bidsdir: str, outputdir: str, workroot: str, subject_label=(), force=Fa
     else:
         print('\n----------------\n'
              f"Done! Now wait for the jobs to finish... Check that e.g. with this command:\n\n  {'qstat - a $(qselect -s RQ)' if manager=='torque' else 'squeue -u '+os.getenv('USER')} | grep fmriprep_sub\n\n"
-              'You can check how much memory and walltime your jobs have used by running:\n\n  hpc_resource_usage.py\n')
+              'You can check how much memory and walltime your jobs have used by running:\n\n  hpc_resource_usage.py\n\n'
+              "After your jobs have finished you can create a group report using BIDScoin's slicereport tool. For instance,\n"
+              'to inspect mean fMRI images you can load the bidscoin/4.2.2 module (or newer) and run:\n\n'
+             f"  slicereport {outputdir} func/*desc-preproc_bold* -x {outputdir}")
 
 
 # Shell usage
