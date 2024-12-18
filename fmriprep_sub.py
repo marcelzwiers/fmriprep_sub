3a019deea1ca5f13444555e24781f991c3c52e3a
MarsMellow <mpzwiers@gmail.com>
2024-12-18 12:26:21 +0100

    Detect the resourcemanager automatically

--- fmriprep_sub.py
+++ fmriprep_sub.py
@@ -15,7 +15,7 @@ from pathlib import Path
 version = os.getenv("FMRIPREP_VERSION")
 
 
-def main(bidsdir: str, outputdir: str, workroot: str, subject_label=(), force=False, manager='torque', mem_mb=20000, walltime=72, file_gb_=50, nthreads=None, argstr='', qargstr='', dryrun=False, skip=True):
+def main(bidsdir: str, outputdir: str, workroot: str, subject_label=(), force=False, mem_mb=20000, walltime=72, file_gb_=50, nthreads=None, argstr='', qargstr='', dryrun=False, skip=True):
 
     # Defaults
     bidsdir   = Path(bidsdir)
@@ -24,6 +24,7 @@ def main(bidsdir: str, outputdir: str, workroot: str, subject_label=(), force=Fa
         outputdir = bidsdir/'derivatives'/'fmriprep'
     if not nthreads:                                        # Set the number of threads between 1 and 8 (but see https://github.com/nipreps/fmriprep/pull/2071)
         nthreads = min(8, max(1, round(mem_mb / 10000)))    # Allocating ~10GB / CPU core
+    manager = 'slurm' if 'slurm' in os.getenv('PATH') else 'torque'
 
     # Map the bids subdirectories
     if not subject_label:
@@ -90,7 +91,7 @@ def main(bidsdir: str, outputdir: str, workroot: str, subject_label=(), force=Fa
                 {fmriprep} {bidsdir} {outputdir} participant -w {workdir} --participant-label {sub_id} {validation} --fs-license-file {licensefile} --mem_mb {mem_mb} --omp-nthreads {nthreads} --nthreads {nthreads} {args}'''
                 .format(pwd         = Path.cwd(),
                         sleep       = 'sleep 1m' if n>1 else '',                                            # Avoid concurrency issues, see: https://neurostars.org/t/updated-fmriprep-workaround-for-running-subjects-in-parallel/6677
-                        fmriprep    = f'apptainer run --cleanenv --bind \$TMPDIR:/tmp {os.getenv("DCCN_OPT_DIR")}/fmriprep/{version}/fmriprep-{version}.simg',
+                        fmriprep    = f'apptainer run --cleanenv --bind \$TMPDIR:/tmp,\$TMPDIR:/var/tmp {os.getenv("DCCN_OPT_DIR")}/fmriprep/{version}/fmriprep-{version}.simg',
                         bidsdir     = bidsdir,
                         outputdir   = outputdir.parent if int(version.split('.')[0])<21 else outputdir,     # Use legacy or bids output-layout (https://fmriprep.org/en/latest/outputs.html#layout)
                         workdir     = workdir,
@@ -122,7 +123,7 @@ def main(bidsdir: str, outputdir: str, workroot: str, subject_label=(), force=Fa
     else:
         print('\n----------------\n'
               'Done! Now wait for the jobs to finish... Check that e.g. with this command:\n\n'
-             f"  {'qstat - a $(qselect -s RQ)' if manager=='torque' else 'squeue -u '+os.getenv('USER')} | grep fmriprep_sub\n\n"
+             f"  {'qstat -a $(qselect -s RQ)' if manager=='torque' else 'squeue -u '+os.getenv('USER')} | grep fmriprep_sub\n\n"
               'You can check how much memory and walltime your jobs have used by running:\n\n'
               '  hpc_resource_usage.py\n\n'
               "After your jobs have finished you can create a group report using BIDScoin's\n"
@@ -148,23 +149,22 @@ if __name__ == "__main__":
                                             '  fmriprep_sub.py /project/3017065.01/bids -w /project/3017065.01/fmriprep_work\n'
                                             '  fmriprep_sub.py /project/3017065.01/bids -o /project/3017065.01/fmriprep --participant_label sub-P010 sub-P018\n'
                                             '  fmriprep_sub.py /project/3017065.01/bids -a " --fs-no-reconall"\n'
-                                            '  fmriprep_sub.py /project/3017065.01/bids -a " --use-aroma --return-all-components --ignore slicetiming --output-spaces MNI152NLin6Asym"\n'
+                                            '  fmriprep_sub.py /project/3017065.01/bids -a "--use-aroma --return-all-components --ignore slicetiming --output-spaces MNI152NLin6Asym"\n'
                                             '  fmriprep_sub.py -f -m 40000 /project/3017065.01/bids -p P018\n\n'
                                             'author:\n'
                                             '  Marcel Zwiers\n ')
     parser.add_argument('bidsdir',                  help='The bids-directory with the (new) subject data')
-    parser.add_argument('-o','--outputdir',         help='The output-directory where the frmiprep output is stored. NB: for fmriprep versions before 21.0.0 the last part of outputdir MUST be named "fmriprep" (e.g. "data/v20.0.0/fmriprep") (default = bidsdir/derivatives/fmriprep)', default='')
+    parser.add_argument('-o','--outputdir',         help='The output-directory where the frmiprep output is stored. NB: for fmriprep versions before 21.0.0 the last part of outputdir MUST be named "fmriprep" (e.g. "derivatives/v20.0.0/fmriprep" instead of "derivatives/v20.0.0") (default = bidsdir/derivatives/fmriprep)', default='')
     parser.add_argument('-w','--workdir',           help='The working-directory where intermediate files are stored (default = a temporary directory', default='')
     parser.add_argument('-p','--participant_label', help='Space seperated list of sub-# identifiers to be processed (the sub- prefix can be removed). Otherwise all sub-folders in the bidsfolder will be processed', nargs='+')
     parser.add_argument('-f','--force',             help='If this flag is given subjects will be processed, regardless of existing folders in the bidsfolder. Otherwise existing folders will be skipped', action='store_true')
     parser.add_argument('-i','--ignore',            help='If this flag is given then already running or scheduled jobs with the same name are ignored, otherwise job submission is skipped', action='store_false')
-    parser.add_argument('-r','--resourcemanager',   help='Resource manager to which the jobs are submitted', choices=('torque', 'slurm'), default='torque', const='torque', nargs='?')
-    parser.add_argument('-m','--mem_mb',            help='Required amount of memory (in mb)', default=20000, type=int)
+    parser.add_argument('-m','--mem_mb',            help='Required amount of memory (in MB)', default=20000, type=int)
     parser.add_argument('-n','--nthreads',          help='Number of compute threads (CPU cores) per job (subject). By default ~10GB/CPU core is allocated, i.e. nthreads = round(mem_mb/10000), but you can increase it to speed up the processing of small datasets (< ~25 subjects), see https://fmriprep.org/en/stable/faq.html#running-subjects-in-parallel', choices=range(1,9), type=int)
     parser.add_argument('-t','--time',              help='Required walltime (in hours)', default=72, type=int)
-    parser.add_argument('-s','--scratch_gb',        help='Required free diskspace of the local temporary workdir (in gb)', default=50, type=int)
-    parser.add_argument('-a','--args',              help='Additional (opaque) arguments that are passed to fmriprep (NB: Use quotes and a leading space to prevent unintended argument parsing)', type=str, default='')
-    parser.add_argument('-q','--qargs',             help='Additional (opaque) arguments that are passed to qsub/sbatch (NB: Use quotes and a leading space to prevent unintended argument parsing)', type=str, default='')
+    parser.add_argument('-s','--scratch_gb',        help='Required free diskspace of the local temporary workdir (in GB)', default=50, type=int)
+    parser.add_argument('-a','--args',              help='Additional (opaque) arguments that are passed to fmriprep (NB: Use quotes and include at least one space character to prevent overearly parsing)', type=str, default='')
+    parser.add_argument('-q','--qargs',             help='Additional (opaque) arguments that are passed to qsub/sbatch (NB: Use quotes and include at least one space character to prevent overearly parsing)', type=str, default='')
     parser.add_argument('-d','--dryrun',            help='Add this flag to just print the fmriprep qsub commands without actually submitting them (useful for debugging)', action='store_true')
     args = parser.parse_args()
 
@@ -178,7 +178,6 @@ if __name__ == "__main__":
              workroot      = args.workdir,
              subject_label = args.participant_label,
              force         = args.force,
-             manager       = args.resourcemanager,
              mem_mb        = args.mem_mb,
              walltime      = args.time,
              nthreads      = args.nthreads,
